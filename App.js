03a382eab6bf55807380417e62902f4009ddac49
u100247 <u100247@MM0012B7.local>
2021-10-09 19:26:44 +0200

    tetrominoes rotation and draw function finished

--- App.js
+++ App.js
@@ -5,6 +5,63 @@ document.addEventListener('DOMContentLoaded', () => {
     const StartBtn  = document.querySelector('#start-button');
     const width = 10;
 
+    const lTetromino = [
+        [1, width+1, width*2+1,2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1,width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ];
+    const zTetromino = [
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1],
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1]
+    ];
+    const tTetromino = [
+        [1,width,width+1,width+2],
+        [1,width+1,width+2,width*2+1],
+        [width,width+1,width+2,width*2+1],
+        [1,width,width+1,width*2+1]
+    ];
+    const oTetromino = [
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1]
+    ];
+    const iTetromino = [
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3]
+    ];
+    
+    const theTetrominoes = [lTetromino,zTetromino, tTetromino, oTetromino, iTetromino];
+
+    let currentPosition = 4;
+    let currentRotation = 0;
+
+    // randomly select a tetromino and its first rotation
+    let random = Math.floor(Math.random()*theTetrominoes.length);
+    console.log(random);
+    let current = theTetrominoes[random][currentRotation];
+
+    // draw the rotation 
+    function draw()  {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('tetromino')
+        })
+    }
+
+    // undraw the rotation 
+    function undraw()  {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino')
+        })
+    }
+
+    draw();
+
 })
 
 //function showAlert(name) {
